13c8990186a24069b281501f4b3adedc44f3dff7
Wan Youwen <1756429307@qq.com>
2018-11-27 18:45:17 +0800

    各种类型转换函数，多一个 四元素 转旋转矩阵

--- Converter.cc
+++ Converter.cc
@@ -1,21 +1,6 @@
 /**
 * This file is part of ORB-SLAM2.
-*
-* Copyright (C) 2014-2016 Raúl Mur-Artal <raulmur at unizar dot es> (University of Zaragoza)
-* For more information see <https://github.com/raulmur/ORB_SLAM2>
-*
-* ORB-SLAM2 is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* ORB-SLAM2 is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with ORB-SLAM2. If not, see <http://www.gnu.org/licenses/>.
+* 各种类型转换函数，多一个 四元素 转旋转矩阵
 */
 
 
@@ -133,7 +118,8 @@ Eigen::Matrix<double,3,3> Converter::toMatrix3d(const cv::Mat &cvMat3)
 
     return M;
 }
-
+	
+// 旋转矩阵转四元素
 std::vector<float> Converter::toQuaternion(const cv::Mat &M)
 {
     Eigen::Matrix<double,3,3> eigMat = toMatrix3d(M);
@@ -147,7 +133,7 @@ std::vector<float> Converter::toQuaternion(const cv::Mat &M)
 
     return v;
 }
-
+//  四元素 转旋转矩阵
 void Converter::RmatOfQuat(cv::Mat &M, const cv::Mat &q) {
   Eigen::Quaterniond _q(q.at<float>(0,3), q.at<float>(0,0), q.at<float>(0,1), q.at<float>(0,2));
   Eigen::Matrix<double,3,3> _m = _q.toRotationMatrix();
