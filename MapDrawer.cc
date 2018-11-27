6f6234f38741623670dfe41388176b9697f73542
Wan Youwen <1756429307@qq.com>
2018-11-27 21:06:46 +0800

    通过深度图生成点云图 滤波 分割平面后 生产 octomap 更新占有格 概率 保存 在胖果林中显示

--- MapDrawer.cc
+++ MapDrawer.cc
@@ -1,21 +1,6 @@
 /**
 * This file is part of ORB-SLAM2.
-*
-* Copyright (C) 2014-2016 Raúl Mur-Artal <raulmur at unizar dot es> (University of Zaragoza)
-* For more information see <https://github.com/raulmur/ORB_SLAM2>
-*
-* ORB-SLAM2 is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* ORB-SLAM2 is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with ORB-SLAM2. If not, see <http://www.gnu.org/licenses/>.
+* 通过 深度图 生成点云图 滤波 分割平面后 生产 octomap 更新占有格 概率 保存 在胖果林中显示
 */
 
 #include "ORBSLAM/include/MapDrawer.h"
