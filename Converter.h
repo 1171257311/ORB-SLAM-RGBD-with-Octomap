1e4ac459400be89a139c0fb2c74b46425c02f9cc
Wan Youwen <1756429307@qq.com>
2018-11-27 18:43:02 +0800

    各种类型转换函数，多一个旋转矩阵 转四元素??

--- Converter.h
+++ Converter.h
@@ -1,21 +1,6 @@
 /**
 * This file is part of ORB-SLAM2.
-*
-* Copyright (C) 2014-2016 Raúl Mur-Artal <raulmur at unizar dot es> (University of Zaragoza)
-* For more information see <https://github.com/raulmur/ORB_SLAM2>
-*
-* ORB-SLAM2 is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* ORB-SLAM2 is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with ORB-SLAM2. If not, see <http://www.gnu.org/licenses/>.
+* 各种类型转换函数，多一个旋转矩阵 转四元素??
 */
 
 #ifndef CONVERTER_H
@@ -50,7 +35,8 @@ public:
     static Eigen::Matrix<double,3,3> toMatrix3d(const cv::Mat &cvMat3);
 
     static std::vector<float> toQuaternion(const cv::Mat &M);
-
+    
+// ===============================new================旋转矩阵 转四元素??
     static void RmatOfQuat(cv::Mat &M, const cv::Mat &q);
 };
 
